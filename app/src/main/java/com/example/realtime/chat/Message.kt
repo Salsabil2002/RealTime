91cc7cfa458ceb3deff573aaaf868552d0b9e0f2
Salsabil2002 <sila2abd@gmail.com>
2023-04-13 21:01:46 +0300

    commit message

---
+++ app/src/main/java/com/example/realtime/chat/Message.kt
@@ -0,0 +1,11 @@
+package com.example.realtime.chat
+
+ data class Message (
+     var textn: String ="",
+     var senderId: String ="",
+     var receiverId :String ="",
+     var timestamp: String =""
+
+ )
+
+
